f33221d6bf06e06939bbba7201b8bbaa0c72684f
Mary-Prabah <mpa22@ic.ac.uk>
2024-12-26 21:41:10 +0000

    Delete Frontend/Example_script.js

--- Frontend/Example_script.js
+++
@@ -1,7 +0,0 @@
-// To contain all the Javascript files (makes the website interactive)
-eel.interact_with_js ("Javascript");
-
-eel.expose (interact_with_py)//I think they are already abstracted, so this makes them comunicate
-function interact_with_py(){
-    console.log("Hello from Python");
-}
