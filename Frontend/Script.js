b423df54f2467a2f0fb228d07a551e2fed685803
Mary-Prabah <mpa22@ic.ac.uk>
2024-12-28 12:10:03 +0000

    Changes made by Mary

--- Frontend/Script.js
+++ Frontend/Script.js
@@ -1,8 +1,8 @@
 const menu = document.querySelector('#mobile-menu'); /*selects as a CSS selector, it returns the first item*/
-const menuLinks = document.querySelectorAll('.navbar__links');
+const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('is.active'); /*classList is an object that represents the list of classes of an element*/
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active'); /*classList is an object that represents the list of classes of an element*/
     menuLinks.classList.toggle('active');  /*add or removes active from the list of classes in menuLinks*/
     /*if .active is present then the menu links disappears */
-})
+});
