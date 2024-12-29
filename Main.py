1265f6fad67f9e288cf72987a382fd52b5cc5844
Mary-Prabah <mpa22@ic.ac.uk>
2024-12-29 16:37:32 +0000

    Changes made by Mary

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
 import eel
 eel.init("Frontend") #initialising our directory
+
+#@eel.expose and then you define the function
+#To call: eel.expose(function_in_java)
+
 eel.start('index.html')
+eel.start('Uploading_page.html')#AT THE END!!
