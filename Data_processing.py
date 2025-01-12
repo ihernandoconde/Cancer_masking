83185511faa51d0b0b4ac329e79feb5d6e406546
Layane Merhi <lm1122@ic.ac.uk>
2025-01-12 22:27:32 +0000

    Rename input and output dirs and fix global runs

--- Data_processing.py
+++ Data_processing.py
@@ -1,5 +1,4 @@
 import pydicom
-from pydicom import pixel_array
 from pydicom.pixel_data_handlers.util import convert_color_space
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +7,7 @@ import numpy as np
 def load_file(path):
     #here or in the UI we should add a try catch to check if the file is actually dicom
     file=pydicom.dcmread(path)  #this reads the data
-    image= pixel_array(file)    #this converts pixel data into an array
+    image= file.pixel_array   #this converts pixel data into an array
     return (file,image)
 def convert_rgb(file,image):
     #for monochrome images
@@ -17,15 +16,15 @@ def convert_rgb(file,image):
         rgb_image=np.stack((image,)*3,axis=-1)
         rgb_image= rgb_image.astype(np.uint8)
     #for YBR images
-    elif filePhotometricInterpretation=='YBR_FULL_422':
+    elif file.PhotometricInterpretation=='YBR_FULL_422':
         rgb_image=convert_color_space(image, current="YBR_FULL_422", desired="RGB")
     #add error for other color values? maybe not here?
     else:
         print('wtf')
     return(rgb_image)
 
-path=r"C:\Users\ihern\Documents\Java_try\breast_masking\image1.dcm"
-file, image=load_file(path)
-rgb_image=convert_rgb(file, image)
-plt.imshow(rgb_image)
-plt.show
+#path=r"C:\Users\ihern\Documents\Java_try\breast_masking\image1.dcm"
+#file, image=load_file(path)
+#rgb_image=convert_rgb(file, image)
+#plt.imshow(rgb_image)
+#plt.show
